07c1e8ecc4cf6f4626c0390d39ad1300ebb5780d
EthanSausville <ethan.sausville@gmail.com>
2018-03-20 16:23:49 -0700

    Upload

---
+++ FirstChallenge/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using FirstChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(ComicBookManager.GetComicBooks());
+        }
+
+        public ActionResult Detail(int id)
+        {
+            return View(ComicBookManager.GetComicBooks().FirstOrDefault(x => x.ComicBookId == id));
+        }
+    }
+}
